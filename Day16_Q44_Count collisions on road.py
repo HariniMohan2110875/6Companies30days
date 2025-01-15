4288e48da25b836ac88db67314568a74b09f212f
HariniMohan2110875 <113509114+HariniMohan2110875@users.noreply.github.com>
2025-01-15 13:50:38 +0000

    c2

---
+++ Day16_Q44_Count collisions on road.py
@@ -0,0 +1,11 @@
+class Solution:
+    def countCollisions(self, d: str) -> int:
+        c = 0
+        c = sum(s!="S" for s in d.lstrip("L").rstrip("R")) 
+        
+        
+        return c
+
+            
+            
+        
